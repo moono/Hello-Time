ca9af58cc8e6174310c64932af2d62bbb7a69def
mookyung song <mookyung@HCs-MacBook-Pro.local>
2015-06-12 13:21:40 +0900

    Initial setup of Hello-Time app
    Initial commit

--- Hello Time/ViewController.m
+++ Hello Time/ViewController.m
@@ -17,11 +17,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self checkTime:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)checkTime:(id)sender
+{
+    // to get current date
+    NSDateFormatter *formatter = [ [NSDateFormatter alloc] init];
+    
+    // set format of date: just time
+    [formatter setDateFormat:@"h:mm:ss a"];
+    
+    // set text to label
+    [_timeLabel setText:[formatter stringFromDate:[NSDate date]]];
+    
+    // tell controller to call this method for every second
+    [self performSelector:@selector(checkTime:) withObject:self afterDelay:1.0];
 }
 
 @end
